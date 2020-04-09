c92e3404c85674cd2df17d398e3fe5b4cfe64917
aysegulkndfr <aysegulkandefer@gmail.com>
2020-04-09 12:14:50 +0300

    Done

--- app/src/main/java/com/udemy/messageapp/MainActivity.java
+++ app/src/main/java/com/udemy/messageapp/MainActivity.java
@@ -3,12 +3,42 @@ package com.udemy.messageapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    List<model> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListeDoldur();
+        tanimla();
+    }
+
+    public void ListeDoldur() {
+        list = new ArrayList<>();
+        model m1 = new model("Ayşegül", "12345");
+        model m2 = new model("Gamze", "67890");
+        model m3 = new model("İlkay", "13579");
+        model m4 = new model("İsmail", "24680");
+        model m5 = new model("İbrahim", "12398");
+
+        list.add(m1);
+        list.add(m2);
+        list.add(m3);
+        list.add(m4);
+        list.add(m5);
+    }
+
+    public void tanimla() {
+        ListView listView = findViewById(R.id.mesajList);
+        adapter adp = new adapter(list, this, this);
+        listView.setAdapter(adp);
     }
 }
---
+++ app/src/main/java/com/udemy/messageapp/model.java
@@ -0,0 +1,28 @@
+package com.udemy.messageapp;
+
+public class model {
+    private String isim;
+    private String tel;
+
+    public model(String isim, String tel) {
+        this.isim = isim;
+        this.tel = tel;
+    }
+
+    public String getIsim() {
+        return isim;
+    }
+
+    public void setIsim(String isim) {
+        this.isim = isim;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+}
+
